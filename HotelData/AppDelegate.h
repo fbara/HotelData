596ff6f90733dc38e92f881476b77a6bf9e62afb
Frank S. Bara <fbara1@yahoo.com>
2016-03-10 12:11:32 -0600

    Initial Commit

---
+++ HotelData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelData
+//
+//  Created by Frank Bara on 3/10/16.
+//  Copyright © 2016 BaraLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
